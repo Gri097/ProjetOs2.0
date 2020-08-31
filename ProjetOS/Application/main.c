18f0ecd61438904f5794b4cdb3b39f93e225303e
Robin <robin@castermane.be>
2020-08-31 17:50:53 +0200

    affichage update

--- ProjetOS/Application/main.c
+++ ProjetOS/Application/main.c
@@ -83,64 +83,64 @@ void afficheTab(struct MemoirePartagee *tabVoitures, int choix){
     triVoiture(pointeurMem->tableauV, 3);
   }
   system("clear");
-  printf("____________________________________________________________________________\n");
+  printf("____________________________________________________________________________________\n");
   switch (choix) {
     case 0:
-    printf("|                              ESSAIS 1                                   |\n");
+    printf("|                              ESSAIS 1                                            |\n");
     break;
     case 3:
-    printf("|                              ESSAIS 2                                   |\n");
+    printf("|                              ESSAIS 2                                            |\n");
     break;
     case 1:
-    printf("|                              ESSAIS 3                                   |\n");
+    printf("|                              ESSAIS 3                                            |\n");
     break;
     case 20:
-    printf("|                          QUALIFICATIONS 1                               |\n");
+    printf("|                          QUALIFICATIONS 1                                        |\n");
     break;
     case 15:
-    printf("|                          QUALIFICATIONS 2                               |\n");
+    printf("|                          QUALIFICATIONS 2                                        |\n");
     break;
     case 10:
-    printf("|                          QUALIFICATIONS 3                               |\n");
+    printf("|                          QUALIFICATIONS 3                                        |\n");
     break;
     case 2:
-    printf("|                           COURSE FINALE                                 |\n");
+    printf("|                           COURSE FINALE                                          |\n");
     break;
   }
   if (choix < 4) {
     choix = 20;
   }
-  printf("|____________________________________________________________________________|\n");
-  printf("| Voiture  |   Tour    |  Section 1  |  Section 2  |  Section 3  | Pit | Out |\n");
-  printf("|----------|-----------|-------------|-------------|-------------|-----|-----|\n");
+  printf("|__________________________________________________________________________________|\n");
+  printf("| Voiture  |   Tour     |   Section 1  |   Section 2  |   Section 3  |  Pit |  Out |\n");
+  printf("|----------|------------|--------------|--------------|--------------|------|------|\n");
   if(finale == 1){
     for(int i = 0; i < choix; i++){
-      printf("|   %2d    |   %.3f\" |   %.3f\"  |   %.3f\"  |   %.3f\"  |  %d |  %d |\n", mem.tableauV[i].numero, (mem.tableauV[i].nbTour), mem.tableauV[i].sections[0], mem.tableauV[i].sections[1], mem.tableauV[i].sections[2], mem.tableauV[i].pit, mem.tableauV[i].out);
-      printf("|----------|----------|-----------|-----------|-----------|-----|-----|\n");
+      printf("|   %2d      |   %.3f\" |    %.3f\"   |    %.3f\"   |    %.3f\"   |   %d  |   %d  |\n", mem.tableauV[i].numero, (mem.tableauV[i].nbTour), mem.tableauV[i].sections[0], mem.tableauV[i].sections[1], mem.tableauV[i].sections[2], mem.tableauV[i].pit, mem.tableauV[i].out);
+      printf("|----------|------------|--------------|--------------|--------------|------|------|\n");
     }
   }
   else{
     for(int i = 0; i < choix; i++){
-      printf("|   %2d    |   %.3f\" |   %.3f\"  |   %.3f\"  |   %.3f\"  |  %d |  %d |\n", mem.tableauV[i].numero, mem.tableauV[i].best[3], mem.tableauV[i].sections[0],mem.tableauV[i].sections[1], mem.tableauV[i].sections[2], mem.tableauV[i].pit, mem.tableauV[i].out);
-      printf("|----------|----------|-----------|-----------|-----------|-----|-----|\n");
+      printf("|   %2d     |   %.3f\" |    %.3f\"   |    %.3f\"   |    %.3f\"   |   %d  |   %d  |\n", mem.tableauV[i].numero, mem.tableauV[i].best[3], mem.tableauV[i].sections[0],mem.tableauV[i].sections[1], mem.tableauV[i].sections[2], mem.tableauV[i].pit, mem.tableauV[i].out);
+      printf("|----------|------------|--------------|--------------|--------------|------|------|\n");
     }
   }
 
-  printf("|  Best   | Section 1 |\n");
-  printf("|---------|-----------|\n");
+  printf("|  Best    | Section 1    |\n");
+  printf("|----------|--------------|\n");
   triVoiture(pointeurMem->tableauV, 0);
-  printf("|   %d    |   %.3f\"  |\n", mem.tableauV[0].numero, mem.tableauV[0].best[0]);
-  printf("|---------|-----------|\n");
-  printf("|  Best   | Section 2 |\n");
-  printf("|---------|-----------|\n");
+  printf("|   %2d     |  %.3f\"     |\n", mem.tableauV[0].numero, mem.tableauV[0].best[0]);
+  printf("|----------|--------------|\n");
+  printf("|  Best    | Section 2    |\n");
+  printf("|----------|--------------|\n");
   triVoiture(pointeurMem->tableauV, 1);
-  printf("|   %d    |   %.3f\"  |\n", mem.tableauV[0].numero, mem.tableauV[0].best[1]);
-  printf("|---------|-----------|\n");
-  printf("|  Best   | Section 3 |\n");
-  printf("|---------|-----------|\n");
+  printf("|   %2d     |  %.3f\"     |\n", mem.tableauV[0].numero, mem.tableauV[0].best[1]);
+  printf("|----------|--------------|\n");
+  printf("|  Best    | Section 3    |\n");
+  printf("|----------|--------------|\n");
   triVoiture(pointeurMem->tableauV, 2);
-  printf("|   %d    |   %.3f\"  |\n", mem.tableauV[0].numero, mem.tableauV[0].best[2]);
-  printf("|---------|-----------|\n");
+  printf("|   %2d     |  %.3f\"     |\n", mem.tableauV[0].numero, mem.tableauV[0].best[2]);
+  printf("|----------|--------------|\n");
 }
 
 void triQualifications(struct MemoirePartagee *tabVoitures, int choix){
